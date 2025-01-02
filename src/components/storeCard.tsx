8f943732eb17818996e68954893a95f9e41fd81b
HenriqueVuolo <henriquevuolo@gmail.com>
2025-01-02 15:09:45 -0300

    fix: update items

--- src/components/storeCard.tsx
+++ src/components/storeCard.tsx
@@ -14,33 +14,33 @@ const ITEMS = [
 	{
 		id: 2,
 		description:
-			'Adds +2 points per click but increases your chance of losing by 2%.',
-		price: 8,
-		pointsPerClickBoost: 2,
+			'Adds +5 points per click but increases your chance of losing by 2%.',
+		price: 15,
+		pointsPerClickBoost: 5,
 		riskBoost: 0.02,
 	},
 	{
 		id: 3,
 		description:
-			'Adds +5 points per click but increases your chance of losing by 3%.',
-		price: 15,
-		pointsPerClickBoost: 5,
-		riskBoost: 0.03,
-	},
-	{
-		id: 4,
-		description:
-			'Adds +50 points per click but increases your chance of losing by 5%.',
-		price: 100,
-		pointsPerClickBoost: 50,
+			'Adds +20 points per click but increases your chance of losing by 5%.',
+		price: 50,
+		pointsPerClickBoost: 20,
 		riskBoost: 0.05,
 	},
 	{
-		id: 5,
+		id: 4,
 		description: 'Decreases your chance of losing by 5%.',
-		price: 200,
+		price: 100,
 		pointsPerClickBoost: 0,
 		riskBoost: -0.05,
+	},
+	{
+		id: 5,
+		description:
+			'Adds +50 points per click but increases your chance of losing by 5%.',
+		price: 150,
+		pointsPerClickBoost: 0,
+		riskBoost: 0.05,
 	},
 ];
 
@@ -50,13 +50,19 @@ function ItemCard({item}: {item: (typeof ITEMS)[0]}) {
 		<div className="flex flex-col p-2 pt-4 gap-4 rounded-lg bg-background min-w-[200px] justify-between">
 			<div className="flex justify-between text-xs font-bold">
 				<div className="rounded-full bg-secondary w-8 h-8 pt-2">
-					<span>{item.pointsPerClickBoost}</span>
+					<span>
+						{item.pointsPerClickBoost > 0 ? '+' : null}
+						{item.pointsPerClickBoost}
+					</span>
 				</div>
 				<div className="rounded-full bg-secondary w-8 h-8 pt-2">
-					<span>{item.riskBoost * 100}%</span>
+					<span>
+						{item.pointsPerClickBoost > 0 ? '+' : null}
+						{item.riskBoost * 100}%
+					</span>
 				</div>
 			</div>
-			<p className="text-pretty text-sm">{item.description}</p>
+			<p className="text-pretty text-sm font-semibold">{item.description}</p>
 			<Button
 				className="max-w-[800px] bg-secondary hover:bg-primary p-2 text-xl font-bold"
 				onClick={() => handleItem(item)}
