9c3a7e217c0208932683ad21d1e90450c1342907
HenriqueVuolo <henriquevuolo@gmail.com>
2025-01-02 15:10:05 -0300

    feat: add font bold to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ export function Button({className, ...props}: ButtonProps) {
 		<button
 			type="button"
 			className={merge(
-				'rounded-lg p-4 w-full max-w-80 self-center bg-background hover:bg-secondary active:bg-primary transition-all',
+				'rounded-lg p-4 w-full max-w-80 self-center font-bold bg-background hover:bg-secondary active:bg-primary transition-all',
 				className
 			)}
 			{...props}
